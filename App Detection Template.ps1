3cc2c55236ba600e638126824225d63bc6c2cb6e
Jack <101634011+Carme99@users.noreply.github.com>
2025-05-15 14:55:07 +0100

    Add files via upload

---
+++ App Detection Template.ps1
@@ -0,0 +1,17 @@
+﻿function Get-InstalledApps
+{
+    if ([IntPtr]::Size -eq 4) {
+        $regpath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    }
+    else {
+        $regpath = @(
+            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+            'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+        )
+    }
+    Get-ItemProperty $regpath | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }} | Select DisplayName
+}
+$results = (Get-InstalledApps | where {$_.DisplayName -EQ "App Name As Shown in Control Panel"}).displayname | Sort-Object
+if($results){
+    write-host "Installed"
+}
